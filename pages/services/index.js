5fdf3e6e46ae8ff786f40dd92fe00bbd05f1c868
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-03-01 21:26:10 +0100

    fetch serviceApi in service page

---
+++ pages/services/index.js
@@ -0,0 +1,25 @@
+
+import Layout from "../../components/layout";
+import { useState, useEffect } from 'react';
+ 
+export function Services(){
+    const [serviceData, setServiceData] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const res = await fetch('https://localhost:7014/api/ServiceApi');
+            const data = await res.json();
+            // console.log(data);
+            setServiceData(data);
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <Layout>
+            <h1> Services page</h1>
+        </Layout>
+    );
+}
+
+export default Services;
