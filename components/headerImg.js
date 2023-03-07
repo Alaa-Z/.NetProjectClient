c28afc7c5dad0cf515ac5f4ded78d9cd7633dbb6
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-03-07 16:21:26 +0100

    change in grid style for services in homepage

--- components/headerImg.js
+++ components/headerImg.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import styles from "./headerImg.module.css";
+import styles from "../styles/headerImg.module.css";
 
 const HeaderImg = () => {
  
--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ function HomePage(){
             </span>
         </h1>
         
-        <div className="p-10 max-w-7xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-20 mx-auto">
+        <div className="p-10 max-w-7xl grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 gap-20 mx-auto">
             {serviceData.map(service => (
                 <Service
                 key={service.id}
