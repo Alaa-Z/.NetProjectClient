76b2176ff3503401ff0148f05caf16e2e981d977
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-03-13 23:01:24 +0100

    fetch data using getServerSideProps

--- pages/services/[serviceId].js
+++ pages/services/[serviceId].js
@@ -1,36 +1,45 @@
 import Nav from "../../components/nav";
 import { useRouter } from "next/router";
 import { useState, useEffect } from 'react';
- 
-export function ServiceDetails(){
 
+export default function ServiceDetails(props){
     const router = useRouter();
-    console.log(router.query.serviceId);
-    const serviceId= router.query.serviceId;
+    const { serviceDetails } = props;
+    const serviceId = router.query.serviceId;
 
-    const serviceApi= "https://localhost:7014/api/ServiceApi/"+ serviceId;
-
-    const [serviceDetails, setServiceDetails] = useState([]);
+    // To render details as HTML
+    const [detailsHtml, setDetailsHtml] = useState("");
 
     useEffect(() => {
-        async function fetchData() {
-            const res = await fetch(serviceApi);
-            const data = await res.json();
-            console.log(data);
-            setServiceDetails(data);
-        }
-        fetchData();
-    }, []);
-
+        // Convert the details string to HTML using dangerouslySetInnerHTML
+        setDetailsHtml({ __html: serviceDetails.details });
+    }, [serviceDetails.details]);
 
     return (
         <>
-        <Nav />
+            <Nav />
             <h1> Details page</h1>
             <p> {serviceDetails.name}</p>
+            <div dangerouslySetInnerHTML={{__html: serviceDetails.details}}></div>
+            <img src={serviceDetails.imagePath} alt={serviceDetails.altText}/>
         </>
-        
     );
 }
 
-export default ServiceDetails;
+// To fetch data at build time 
+export async function getServerSideProps(context){
+    const { query: { serviceId } } = context;
+    // I added this line after I got a self signed certificate error 
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    const serviceApi= `https://localhost:7014/api/ServiceApi/${serviceId}`;
+    const res = await fetch(serviceApi);
+    const serviceDetails = await res.json();
+    const url = new URL(serviceDetails.imagePath, serviceApi);
+    serviceDetails.imagePath = url.toString();
+    console.log(serviceDetails);
+    return {
+        props: {
+            serviceDetails
+        }
+    }
+}
