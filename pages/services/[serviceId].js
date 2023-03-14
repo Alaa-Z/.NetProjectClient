d85b8a175c68d3d1570d5a0d71a9cc56ed74358e
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-03-14 10:24:22 +0100

    add style and animation to serviceId page

--- pages/services/[serviceId].js
+++ pages/services/[serviceId].js
@@ -1,6 +1,7 @@
 import Nav from "../../components/nav";
 import { useRouter } from "next/router";
-import { useState, useEffect } from 'react';
+import Typed from 'typed.js';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function ServiceDetails(props){
     const router = useRouter();
@@ -9,19 +10,49 @@ export default function ServiceDetails(props){
 
     // To render details as HTML
     const [detailsHtml, setDetailsHtml] = useState("");
+    const titleRef = useRef(null);
 
     useEffect(() => {
         // Convert the details string to HTML using dangerouslySetInnerHTML
         setDetailsHtml({ __html: serviceDetails.details });
-    }, [serviceDetails.details]);
+      
+        // To animate the name of the service 
+        const typed = new Typed(titleRef.current, {
+          strings:  [serviceDetails.name],
+          typeSpeed: 80,
+          backSpeed: 50,
+          loop: false,
+          showCursor: false // to hide the cursor
+        });
+      
+        return () => {
+          typed.destroy();
+        }
+      }, []);
+      
+
 
     return (
         <>
-            <Nav />
-            <h1> Details page</h1>
-            <p> {serviceDetails.name}</p>
-            <div dangerouslySetInnerHTML={{__html: serviceDetails.details}}></div>
-            <img src={serviceDetails.imagePath} alt={serviceDetails.altText}/>
+        <Nav />
+            <hr className="bg-pink-600 py-4 px-8 ">
+            </hr>
+            <div className="mx-auto py-8 px-4">
+                <div className="md:w-1/2 pr-8 w-full mx-auto mt-8 mb-8">
+                    <h1 ref={titleRef} 
+                        className="flex justify-center mt-8 mb-8 font-bold sm:text-xl sm:leading-relaxed relative">
+                        {(serviceDetails.name)}
+                    </h1>
+                    <div className="max-w-3xl mx-auto" 
+                        dangerouslySetInnerHTML={{__html: serviceDetails.details}}
+                        >
+                    </div>
+                </div>
+
+                <div className="md:w-1/2 mx-auto ">
+                    <img src={serviceDetails.imagePath} alt={serviceDetails.altText} className="w-full object-cover max-h-96" />
+                </div>
+            </div>
         </>
     );
 }
@@ -31,12 +62,14 @@ export async function getServerSideProps(context){
     const { query: { serviceId } } = context;
     // I added this line after I got a self signed certificate error 
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    // API for get a service 
     const serviceApi= `https://localhost:7014/api/ServiceApi/${serviceId}`;
     const res = await fetch(serviceApi);
     const serviceDetails = await res.json();
+    // To get image path
     const url = new URL(serviceDetails.imagePath, serviceApi);
     serviceDetails.imagePath = url.toString();
-    console.log(serviceDetails);
+    // console.log(serviceDetails);
     return {
         props: {
             serviceDetails
